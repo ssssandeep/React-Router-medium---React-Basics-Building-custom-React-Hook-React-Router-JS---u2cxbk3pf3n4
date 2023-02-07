ee9a264e31b72f33f4dcf0e2c1e38d02218ddaa9
ssssandeep <ssssandeep57@gmail.com>
2023-02-07 17:15:09 +0000

    updated

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 
 export const BackgroundColorChanger = () =>{
+    const {colorname} = useParams();
     return(
-       
+       <div style={{height:'100vh', backgroundColor:colorname}}>
+        Background Color Changer
+       </div>
     )
 }
--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 
 export const Calculator = () =>{
-
+    const [searchParams] = useSearchParams();
+    const num1 = searchParams.get('num1')
+    const num2 = searchParams.get('num2')
+    const op = searchParams.get('op')
+    const x  =decodeURIComponent(window.location.search)
+    console.log(num1, num2, op, x);
+    const getResult = () =>{
+        if(op=== '+'){
+            return num1+num2;
+        }else if(op==='-'){
+            return num1-num2;
+        }else if(op === '*'){
+            return num1*num2;
+        }else if(op === '/'){
+            return num1/num2;
+        }
+        return 1;
+    }
     return(
-        
+        <div>
+            Your calculation Result is <span className="calc-result">{getResult()}</span>
+        </div>
     )
 }
